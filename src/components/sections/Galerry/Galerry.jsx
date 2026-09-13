38630430ba2cc57eb9bee5d5b7ae75abacba7e54
MuhammadAdrianEfrichan <adrianefrich@gmail.com>
2026-09-13 12:56:00 +0700

    done

--- src/components/sections/Galerry/Galerry.jsx
+++ src/components/sections/Galerry/Galerry.jsx
@@ -21,7 +21,7 @@ const GalerryData = [
 
 
 const Galerry = ()=>{
-    return <section className="relative flex min-h-[32rem] flex-col items-center justify-center gap-10 bg-cover bg-center bg-no-repeat px-5 py-16 sm:px-10 sm:py-24 lg:h-161.25 lg:gap-15 lg:px-14.75 lg:py-32.75"
+    return <section className="relative flex min-h-128 flex-col items-center justify-center gap-10 bg-cover bg-center bg-no-repeat px-5 py-16 sm:px-10 sm:py-24 lg:h-161.25 lg:gap-15 lg:px-14.75 lg:py-32.75"
             style={{ backgroundImage: `url(${galeriHero})` }}>
                 <img src={galeri} alt="" className="" />
                 <div className="grid w-full max-w-7xl grid-cols-1 gap-5 sm:grid-cols-3">
--- src/components/sections/Hero/Hero.jsx
+++ src/components/sections/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = ()=>{
 
     return <section>
         <div className="relative">
-            <img src={hero} alt="hero" className="min-h-[32rem] w-full object-cover object-center sm:min-h-[38rem] lg:min-h-0" />
+            <img src={hero} alt="hero" className="min-h-128 w-full object-cover object-center sm:min-h-152 lg:min-h-0" />
             <div className="absolute inset-0 flex flex-col items-center justify-center px-6 text-center">
             <img src={heroLine} alt="" className="w-48 sm:w-72 lg:w-auto" />
             <img src={ilustrasiHero} alt="" className="w-full max-w-2xl py-10 sm:py-15"/>
